7be32c542ba4ad1de47622131e31ff297e00bf81
Imam-f <mohamadimamfirdaus17@gmail.com>
2021-08-13 14:35:35 +0700

    Set main to switch from clear and red

--- TCS230_vscode_gcc_arm_nucleo_f411re/TCS230/main.cpp
+++ TCS230_vscode_gcc_arm_nucleo_f411re/TCS230/main.cpp
@@ -59,7 +59,7 @@ void time() {
 
         //color++;
         //if (color > 3) color = 0;
-        color = color == 3 ? 1 : 3 ;
+        color = color == 2 ? 1 : 2 ;
         setColor = color;
         
         wait(0.5);
@@ -70,15 +70,13 @@ void time() {
 
 
 int main() {
-
     in.mode(PullDown); // Set the pin to Pull Down mode.
-    wait(1);
+    wait_ms(1000);
     n = 0;
-    color = 0;
+    color = 1;
     setColor = color;
 
-    s0 = 0;
-    s1 = 1;         // Frequency 2% = 12 kHz full-scale.
+    s0 = 0; s1 = 1;         // Frequency 2% = 12 kHz full-scale.
 
     in.rise(&time);  // Set up the interrupt for rising edge
     t.start();       // Start the timer
